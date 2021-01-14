72c9c3867e5a043df35508987c9fc22e88df77fe
Mathias Parking <cph-mp525@cphbusiness.dk>
2021-01-14 18:22:55 +0100

    added exam url

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const mainURL = "http://localhost:8080/jpareststarter";
+const mainURL = "https://mparking.dk/ProgExamBackend/";
 const userInfoEndpoint = "/api/info/user";
 const adminInfoEndpoint = "/api/info/admin";
 const defaultEndpoint = "/api/default";
